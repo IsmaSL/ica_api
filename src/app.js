73a1cbc433bbdc45cfa54b13cf0dd749ed7f5b13
Ismael SL <ucmex_isma@hotmail.com>
2023-10-16 23:51:33 -0600

    Login function added

--- src/app.js
+++ src/app.js
@@ -1,18 +1,42 @@
 import express from "express";
 import { pool } from "./database.js";
 import { PORT } from "./config.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Inicializa express
 const app = express()
+
+app.use(express.json()); // Para poder recibir datos JSON en el body
 
 app.get('/', (req, res) => {
     res.send('Welcome to my Sever');
 })
 
-app.get('/ping', async (req, res) => {
-    const [result] = await pool.query(`SELECT "hello world" as RESULT`);
-    res.json(result[0])
-})
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).send({ error: 'Faltan datos de usuario o contraseña' });
+    }
+
+    const [rows] = await pool.query('SELECT * FROM users WHERE correo = ?', [username]);
+    if (rows.length === 0) {
+        return res.status(401).send({ error: 'Usuario o contraseña incorrectos' });
+    }
+
+    const user = rows[0];
+    
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        return res.status(401).send({ error: 'Usuario o contraseña incorrectos' });
+    }
+
+    // Aquí puedes usar JWT para generar un token y enviarlo al cliente si así lo prefieres.
+    const token = jwt.sign({ userId: user.id }, 'batichico', { expiresIn: '1h' });
+
+    res.send({ user, token });
+});
 
 app.get('/all-users', async (req, res) => {
     const [rows] = await pool.query('SELECT * FROM users;');
