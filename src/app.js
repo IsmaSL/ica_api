2972c282bcb24005f6164f14d1dc965727087f08
Ismael SL <ucmex_isma@hotmail.com>
2023-10-16 22:41:46 -0600

    First commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { pool } from "./database.js";
+import { PORT } from "./config.js";
+
+// Inicializa express
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my Sever');
+})
+
+app.get('/ping', async (req, res) => {
+    const [result] = await pool.query(`SELECT "Hello world" as RESULT`);
+    res.json(result[0]);
+    console.log(result[0]);
+})
+
+app.get('/allUsers', async (req, res) => {
+    const [rows] = await pool.query('SELECT * FROM users;');
+    res.json(rows);
+})
+
+app.get('/addUser', async (req, res) => {
+    const result = await pool.query('INSERT INTO users (correo, contraseña, url_img, nombre, apellidos, telefono, rol) VALUES ()');
+})
+
+
+// Da un puerto
+app.listen(PORT)
+console.log('Server on port ', PORT)
